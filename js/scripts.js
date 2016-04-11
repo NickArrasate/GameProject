8ef68384e7f4132bb6df9671d90068f2fd631b62
Nick <narrasate22@hotmail.com>
2016-04-11 07:06:36 -0700

    add an introduction.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,11 @@
 
 
 // Global Variables ==========================================
-var place = null; //used to index through room arrays. (rooms1,rooms2)
+var place = null; //used to index through room arrays. (roomCenter,roomRight)
 var arrayPlace = null; //used to index through array of rooms. (roomArray)
-var rooms1 = [];
-var rooms2 = [];
+var roomCenter = [];
+var roomRight = [];
+var roomLeft = [];
 var roomArray = [];
 // var Character = null;
 var directions = null;
@@ -15,25 +16,32 @@ function Character(health, strength, sanity, items){
  this.sanity = sanity;
  this.items= items;
 };
-Character.prototype.addSanity1 = function(){
-  return this.sanity += 1;
+Character.prototype.addSanity = function(amount){
+  return this.sanity += amount;
 };
-Character.prototype.loseSanity1 = function(){
-  return this.sanity -= 1;
+Character.prototype.loseSanity = function(amount){
+  return this.sanity -= amount;
+};
+Character.prototype.addHealth = function(amount){
+  return this.health += amount;
+};
+Character.prototype.loseHealth = function(amount){
+  return this.health -= amount;
 };
 var Character = new Character(100,10,10,['Gold Lighter']);
 // user interface logic ========================================
 // Setup the rooms array and starting location and stats========================
 $(document).ready(function(){
 
-  var rooms1 = [turnback, path, entrance, foyer, hallway1];// y-axis array================
-  var rooms2 = [null, null, terrace];// x-axis array ===========================
-  var roomArray = [rooms1,rooms2]//array for both y- and x-axis==============================
-  roomArray.push['rooms1','rooms2'];
+  var roomCenter = [turnback, introduction,path, entrance, foyer, hallway1, hallway2];// y-axis array================
+  var roomRight = [null,null,null, terrace];// x-axis array ===========================
+  var roomLeft = [null,null,null,null,null,null,labratory];
+  var roomArray = [roomLeft,roomCenter,roomRight];//array for both y- and x-axis==============================
+  console.log(roomArray);
   var place = 1;
-  var arrayPlace = 0;
+  var arrayPlace = 1;
   $('#room-display').append(roomArray[arrayPlace][place].description);
-  characterRefresh(Character);
+
 
   // movement and setting=====================================================
   $('.directions').click(function() {
@@ -54,13 +62,12 @@ $(document).ready(function(){
     $('.directions').hide();
     console.log(roomArray[arrayPlace][place].directions);//log to track our coords in console.===================
     directionCheck(roomArray[arrayPlace][place].directions);
+});
 //Contextual button function. pressing the contextual button calls the rooms 'after' function======================================================
   $('#contextual').click(function(){
     roomArray[arrayPlace][place].after(Character);
-    characterRefresh(Character);
     directionCheck(roomArray[arrayPlace][place].directions);
   });
-});
 });
 // Business logic=======================================
 // protoypes for updating character stats. Call the proto in the room object functions.
@@ -107,16 +114,46 @@ function arrayChanger(direction){
 
 // Room objects to append into display ======================================
 // Rooms should contain a 'description' to be appended into html, an 'action' function to happen when char moves into room (can be null), an 'after' function to run after the 'contextual' button has been pressed and the available 'directions' from the room.=============================================
+var labratory = {
+  description: '<div class="room" id="labratory">' +
+  '<p>' + 'You make your way down the passage to the left. After walking for a bit, you come across two doors opposite each other. The door on the left is locked tight. However, the door on the right is slightly ajar. Will you investigate?' + '</p>' +
+  '</div>',
+  action: function(){
+  $('#contextual').show();
+  $('#contextual span.buttontext').append('Push the door open.');
+},
+  after: function(Character){
+    Character.loseSanity(3);
+    characterRefresh(Character);
+    $('#contextual').hide();
+    $('#contextual span.buttontext').empty();
+    $('#room-display').empty();
+    $('#room-display').append(
+    '<div class="room" id="labratory">' +
+  '<p>' + 'The room is small and contains several tables densely cluttered with glass vials and scientific equipment. Dust covers every surface. You approach a large glass container. When you hold your light up to it, the shape of a large, curled tentacle emerges through the cloudy liquid that suspends it. You recoil in disgust at the sight and knock a vial off the table behind you sending it to the ground. The sound of it shattering in the silence is enough to send your heart racing. You exit the labratory in a hurry, eager to put some distance between you and the odd tentacle in the jar...' + '</p>' +
+  '</div>');
+},
+directions: ['up','down']
+}
+var hallway2 = {
+  description: '<div class="room" id="hallway2">' +
+  '<p>' + 'As you reach the bottom of the stairs, the the air smells more and more fetid. The weak flame in your hand is the only light. There is a path to your left and one to your right, each indiscernible from the other in the darkness...' + '</p>' +
+  '</div>',
+  action: function(){},
+  after: null,
+  directions: ['left','right'],
+}
 var hallway1 = {
   description: '<div class="room" id="hallway1">' +
-  '<p>' + 'As you enter the stairway, you feel a chill wind rise to greet you. The air has an old smell about it as if it has been laying still for eons. The stairs before you plunge into darkness ahead of you. If only there was an item to light help the way...' + '</p>' +
+  '<p>' + 'As you enter the stairway, you feel a chill wind rise to greet you. The air has an old smell about it as if it has been laying still for eons. The stairs before you plunge into darkness. If only there was an item to light help the way...' + '</p>' +
   '</div>',
   action: function(){
     $('#contextual').show();
     $('#contextual span.buttontext').append('Light the <span class ="item">gold lighter</span>.');
   },
   after: function(Character){
-    Character.addSanity1();
+    Character.addSanity(1);
+    characterRefresh(Character);
     $('#contextual').hide();
     $('#contextual span.buttontext').empty();
     hallway1.directions.push("up");
@@ -134,17 +171,18 @@ var foyer = {
   '<p>' + 'You scraped your back on the rusty gate as you passed beneath (-1 <span class = "health">health</span>). You find yourself in a small, dim vestibule. There is a stairway up ahead... ' + '</p>' +
   '</div>',
   action: function(Character){
-    Character.health -= 1;
+    Character.loseHealth(1);
+    characterRefresh(Character);
   },
   after: null,
   directions: ['up'],
 }
 var terrace = {
   description: '<div class="room" id="entrance">' +
-  '<p>' + 'You find yourself on a small terrace, the wind blows eerily, something\'s not right...'  + '</p>' +
+  '<p>' + 'You find yourself on a small terrace, the wind moans through the trees. The shadows beneath the limbs deepen and you feel something watching you. Your skin crawls and you wonder if you might just be imagining things...'  + '</p>' +
   '</div>',
   action: function(Character){
-    Character.loseSanity1();
+    Character.loseSanity(2);
   },
   after: null,
   directions: ['left'],
@@ -177,4 +215,17 @@ var turnback = {
   action: function(){},
   after: null,
   directions: ['up'],
+}
+var introduction = {
+  description: '<div class="room" id="introduction">' +
+  '<p>' + 'Welcome to our game. You have been sent by a wealthy, anonymous sponsor to explore a long forgotten cathedral. St. Lovecraft\'s Cathedral was lost in a great flood many years ago and only recently, due to the considerable efforts of your sponsor, became accessible. You have been asked to return anything you may find inside. There are many rooms and places to explore and you have been provided with a set of coordinates to track your progress. Please note that your statistics and a list of your held items may be found on the lower right of your screen. Enjoy.' + '</p>' +
+  '</div>',
+  action: function(){},
+  after: function(){
+    $('#contextual').hide();
+    $('#contextual span.buttontext').empty();
+    characterRefresh(Character);
+    this.directions.push('up')
+  },
+  directions: [],
 }
